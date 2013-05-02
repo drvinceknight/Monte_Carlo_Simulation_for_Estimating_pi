bf080002738c3d65ed582b935e3623f5878010da
drvinceknight <vincent.knight@gmail.com>
2013-05-02 15:25:26 +0100

    New pi estimator written

--- Estimate_pi.py
+++ Estimate_pi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 from random import random
+import matplotlib.pyplot as plt
 """
 Script to simulate rain in a square field. Counting the number of rain drops in the inscribed circle of radius equal to the length of the field. The ratio of the number of drops in the circle to the total number of drops gives $\pi$.
 """
@@ -17,16 +18,32 @@ def is_point_in_circle(point, length_of_field=1):
     """
     Return True if point is in inscribed circle
     """
-    return (point[0]) ** 2 + (point[1]) ** 2 <= length_of_field / 2
+    return (point[0]) ** 2 + (point[1]) ** 2 <= (length_of_field / 2) ** 2
 
 
-def rain(number_of_drops=1000, length_of_field=2, plot=True, format='pdf'):
+def plot_rain_drops(drops_in_circle, drops_out_of_circle, length_of_field=1, format='pdf'):
+    """ Function to draw rain drops """
+    number_of_drops_in_circle = len(drops_in_circle)
+    number_of_drops_out_of_circle = len(drops_out_of_circle)
+    number_of_drops = number_of_drops_in_circle + number_of_drops_out_of_circle
+    plt.figure()
+    plt.xlim(-length_of_field / 2, length_of_field / 2)
+    plt.ylim(-length_of_field / 2, length_of_field / 2)
+    plt.scatter([e[0] for e in drops_in_circle], [e[1] for e in drops_in_circle], color='blue', label="Drops in circle")
+    plt.scatter([e[0] for e in drops_out_of_circle], [e[1] for e in drops_out_of_circle], color='black', label="Drops out of circle")
+    plt.legend(loc="center")
+    plt.title("%s darts thrown: %s landed in circle, estimating $\pi$ as %.4f." % (number_of_drops, number_of_drops_in_circle, 4 * number_of_drops_in_circle / number_of_drops))
+    plt.savefig("%s_darts.%s" % (number_of_drops, format))
+
+
+def rain(number_of_drops=1000, length_of_field=1, plot=True, format='pdf', dynamic=False):
     """
-    Function to throw darts.
+    Function to make rain drops.
     """
     number_of_drops_in_circle = 0
     drops_in_circle = []
     drops_out_of_circle = []
+    pi_estimate = []
     for k in range(number_of_drops):
         d = (rain_drop(length_of_field))
         if is_point_in_circle(d, length_of_field):
@@ -34,30 +51,35 @@ def rain(number_of_drops=1000, length_of_field=2, plot=True, format='pdf'):
             number_of_drops_in_circle += 1
         else:
             drops_out_of_circle.append(d)
-    if plot:
+        if dynamic:
+            print "Plotting drop number: %s" % k
+            plot_rain_drops(drops_in_circle, drops_out_of_circle, length_of_field, format)
+        pi_estimate.append(4 * number_of_drops_in_circle / (k + 1))
+    # Plot the pi estimates
+    plt.figure()
+    plt.scatter(range(1, number_of_drops + 1), pi_estimate)
+    plt.title("$\pi$ estimate against number of rain drops")
+    plt.xlabel("Number of rain drops")
+    plt.ylabel("$\pi$")
+    plt.savefig("Pi_estimate_for_%s_drops_thrown.pdf" % number_of_drops)
+
+    if plot and not dynamic:
         # If the plot option is passed and matplotlib is installed this plots
-        import matplotlib.pyplot as plt
-        plt.figure()
-        plt.xlim(-length_of_field / 2, length_of_field / 2)
-        plt.ylim(-length_of_field / 2, length_of_field / 2)
-        plt.scatter([e[0] for e in drops_in_circle], [e[1] for e in drops_in_circle], color='blue', label="Drops in circle")
-        plt.scatter([e[0] for e in drops_out_of_circle], [e[1] for e in drops_out_of_circle], color='black', label="Drops out of circle")
-        plt.legend(loc="center")
-        plt.title("%s darts thrown: %s landed in circle, estimating $\pi$ as %s." % (number_of_darts, number_of_drops_in_circle, 4 * number_of_drops_in_circle / number_of_darts))
-        plt.savefig("%s_darts.%s" % (number_of_darts, format))
+        plot_rain_drops(drops_in_circle, drops_out_of_circle, length_of_field, format)
 
     return [number_of_drops_in_circle, number_of_drops]
 
 
 if __name__ == "__main__":
     from sys import argv
-    number_of_darts = 100
+    number_of_drops = 100
     if len(argv) > 1:
-        number_of_darts = eval(argv[1])
-    r = rain(number_of_darts, plot=True, format='png')
+        number_of_drops = eval(argv[1])
+    r = rain(number_of_drops, plot=True, format='pdf', dynamic=True)
+    #r = rain(number_of_drops, plot=True, format='png', dynamic=False)
     # Print to screen:
     print "----------------------"
-    print "%s darts thrown" % number_of_darts
+    print "%s darts thrown" % number_of_drops
     print "pi estimated as:"
     print "\t%s" % (4 * r[0] / r[1])
     print "----------------------"
