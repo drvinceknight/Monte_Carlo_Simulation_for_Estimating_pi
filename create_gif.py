6bb114f2f8e58a05814b04fc4a99f11157b45dab
drvinceknight <vincent.knight@gmail.com>
2013-05-02 15:35:06 +0100

    Don't add estimate graph to gif

--- create_gif.py
+++ create_gif.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
     directory = sys.argv[1]
 
 files = os.listdir(directory)
-pdf_files = [f for f in files if ".pdf" in f]
+pdf_files = [f for f in files if "darts.pdf" in f]
 file_lengths = [len(f) for f in pdf_files]
 max_file_length = max(file_lengths)
 print "%s pdf files found" % len(pdf_files)
